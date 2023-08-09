7e19d7d26b01a381ffbd1eefad23268165cd512d
김인태 <dlsxody1@naver.com>
2023-08-09 19:37:43 +0900

    TODO : react-query refecth 어떻게 사용할지 연구

--- React-Only-CSS/src/App.jsx
+++ React-Only-CSS/src/App.jsx
@@ -1,35 +1,92 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./style/todo.css";
+import { useEffect, useState } from "react";
+import supabase from "./lib/supabase/supabase";
+import { useQuery } from "react-query";
+// eslint-disable-next-line no-unused-vars
 
 function App() {
-  const [count, setCount] = useState(0)
+  const { addTodo, deleteTodo, getTodo, updateTodo } = supabase;
+  const [inputData, setInputData] = useState("");
+  const [todoToggle, setTodoToggle] = useState(false);
+  const { data, refetch } = useQuery(["todo"], getTodo, {
+    refetchOnMount: true,
+  });
+
+  // issue 발생
+  // todo를 삭제했을 때 refetch가 안됨
+  // 시도 1.
+
+  useEffect(() => {
+    refetch();
+  }, [data]);
+
+  const onChangeValue = (e) => {
+    setInputData(e.target.value);
+  };
+
+  const onSubmitValue = (e, todo) => {
+    e.preventDefault();
+    addTodo(inputData);
+    console.log("데이터 잘 들어갑니다.");
+  };
+
+  const modifyTodo = () => {
+    setTodoToggle((prev) => !prev);
+    console.log(todoToggle);
+  };
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <div className="wrapper">
+          <div className="todo__container">
+            <div className="todo__list__container">
+              <div className="todo__list__title">TODO LIST</div>
+              <form
+                className="todo__form"
+                onSubmit={(e) => onSubmitValue(e, inputData)}
+              >
+                <input
+                  className="todo__input"
+                  type="text"
+                  placeholder="할 일을 입력하세요"
+                  onChange={onChangeValue}
+                />
+                <button className="todo__button">추가</button>
+              </form>
+              <div className="todo__list">
+                <div className="todo">
+                  {data?.map((el) => {
+                    return (
+                      <div className="todo__box" key={el.id} name={el.id}>
+                        <div>{el.todo}</div>
+                        <div className="button__box">
+                          <button
+                            className="button__modify"
+                            onClick={modifyTodo}
+                          >
+                            수정
+                          </button>
+                          <button
+                            className="button__delete"
+                            onClick={() => {
+                              deleteTodo(el.id);
+                            }}
+                          >
+                            삭제
+                          </button>
+                        </div>
+                      </div>
+                    );
+                  })}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ React-Only-CSS/src/lib/supabase/supabase.js
@@ -0,0 +1,42 @@
+import { createClient } from "@supabase/supabase-js";
+
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
+const SUPABASE_API_KEY = import.meta.env.VITE_SUPABASE_API_KEY;
+
+const supabase = createClient(SUPABASE_URL, SUPABASE_API_KEY);
+
+const getTodo = async () => {
+  const { data: todo, error } = await supabase.from("reacttodo").select();
+  console.log("요청함");
+  if (error) throw error;
+  //DB에서 에러가 발생했을 때 에러를 받기 위함.
+  return todo;
+};
+
+const addTodo = async (todo) => {
+  const { error } = await supabase
+    .from("reacttodo")
+    .insert([{ todo: todo }])
+    .select();
+  //insert문을 쓰고나서 select문을 사용한 이유는
+  //데이터가 잘 들어갔는지 확인하기 위함.
+  if (error) throw error;
+};
+
+const deleteTodo = async (id) => {
+  const { error } = await supabase.from("reacttodo").delete().eq("id", id);
+
+  if (error) throw error;
+};
+
+const updateTodo = async (id) => {
+  const { error } = await supabase
+    .from("reacttodo")
+    .update({
+      id,
+    })
+    .select();
+  if (error) throw error;
+};
+
+export default { updateTodo, addTodo, deleteTodo, getTodo };
